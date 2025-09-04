eada16764ede78407190486d0d2a6c7e97507eef
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-09-04 15:24:56 +0800

    done fixing editing approver

--- app/Http/Controllers/CorrectiveActionRequestController.php
+++ app/Http/Controllers/CorrectiveActionRequestController.php
@@ -153,75 +153,122 @@ class CorrectiveActionRequestController extends Controller
             $corrective_action->save();
         }
 
-        $corrective_action_request_approver = CorrectiveActionRequestApprover::where('corrective_action_request_id', $id)->get();
-        if ($corrective_action_request_approver->isEmpty())
+        $corrective_action_request_approver = CorrectiveActionRequestApprover::where('corrective_action_request_id', $id)->delete();
+        $audit_head = User::where('role_id',4)->first();
+        if ($car->department_id == 2)
         {
-            $audit_head = User::where('role_id',4)->first();
-            if ($car->department_id == 2)
-            {
-                $approver_array = [
-                    $car->auditee_id,
-                    $car->auditor_id
-                ];
-            }
-            else
-            {
-                $approver_array = [
-                    $car->auditee_id,
-                    $car->auditor_id,
-                    $audit_head->id,
-                ];
-            }
-            
-            if ($car->department_id == 2)
-            {
-                $users = User::whereIn('id', $approver_array)->orderBy('role_id','desc')->get();
-            }
-            else
-            {
-                $users = User::whereIn('id', $approver_array)->orderBy('level','asc')->get();
-            }
-            
-            foreach($users as $key=>$user)
-            {
-                $corrective_action_request_approver = new CorrectiveActionRequestApprover;
-                $corrective_action_request_approver->user_id = $user->id;
-                $corrective_action_request_approver->corrective_action_request_id = $id;
-                $corrective_action_request_approver->level = $key+1;
-                if ($key == 0)
-                {
-                    $corrective_action_request_approver->status = 'Submitted';
-                }
-                elseif($key==1)
-                {
-                    $corrective_action_request_approver->status = 'Pending';
-                }
-                else
-                {
-                    $corrective_action_request_approver->status = 'Waiting';
-                }
-                $corrective_action_request_approver->save();
-            }
+            $approver_array = [
+                $car->auditee_id,
+                $car->auditor_id
+            ];
         }
         else
         {
-            foreach($corrective_action_request_approver as $key=>$approver)
-            {
-                if ($key == 0)
-                {
-                    $approver->status = 'Submitted';
-                }
-                elseif($key==1)
-                {
-                    $approver->status = 'Pending';
-                }
-                else
-                {
-                    $approver->status = 'Waiting';
-                }
-                $approver->save();
-            }
+            $approver_array = [
+                $car->auditee_id,
+                $car->auditor_id,
+                $audit_head->id,
+            ];
         }
+            
+        if ($car->department_id == 2)
+        {
+            $users = User::whereIn('id', $approver_array)->orderBy('role_id','desc')->get();
+        }
+        else
+        {
+            $users = User::whereIn('id', $approver_array)->orderBy('level','asc')->get();
+        }
+        
+        foreach($users as $key=>$user)
+        {
+            $corrective_action_request_approver = new CorrectiveActionRequestApprover;
+            $corrective_action_request_approver->user_id = $user->id;
+            $corrective_action_request_approver->corrective_action_request_id = $id;
+            $corrective_action_request_approver->level = $key+1;
+            if ($key == 0)
+            {
+                $corrective_action_request_approver->status = 'Submitted';
+            }
+            elseif($key==1)
+            {
+                $corrective_action_request_approver->status = 'Pending';
+            }
+            else
+            {
+                $corrective_action_request_approver->status = 'Waiting';
+            }
+            $corrective_action_request_approver->save();
+        }
+
+        // if ($corrective_action_request_approver->isEmpty())
+        // {
+        //     $audit_head = User::where('role_id',4)->first();
+        //     if ($car->department_id == 2)
+        //     {
+        //         $approver_array = [
+        //             $car->auditee_id,
+        //             $car->auditor_id
+        //         ];
+        //     }
+        //     else
+        //     {
+        //         $approver_array = [
+        //             $car->auditee_id,
+        //             $car->auditor_id,
+        //             $audit_head->id,
+        //         ];
+        //     }
+            
+        //     if ($car->department_id == 2)
+        //     {
+        //         $users = User::whereIn('id', $approver_array)->orderBy('role_id','desc')->get();
+        //     }
+        //     else
+        //     {
+        //         $users = User::whereIn('id', $approver_array)->orderBy('level','asc')->get();
+        //     }
+            
+        //     foreach($users as $key=>$user)
+        //     {
+        //         $corrective_action_request_approver = new CorrectiveActionRequestApprover;
+        //         $corrective_action_request_approver->user_id = $user->id;
+        //         $corrective_action_request_approver->corrective_action_request_id = $id;
+        //         $corrective_action_request_approver->level = $key+1;
+        //         if ($key == 0)
+        //         {
+        //             $corrective_action_request_approver->status = 'Submitted';
+        //         }
+        //         elseif($key==1)
+        //         {
+        //             $corrective_action_request_approver->status = 'Pending';
+        //         }
+        //         else
+        //         {
+        //             $corrective_action_request_approver->status = 'Waiting';
+        //         }
+        //         $corrective_action_request_approver->save();
+        //     }
+        // }
+        // else
+        // {
+        //     foreach($corrective_action_request_approver as $key=>$approver)
+        //     {
+        //         if ($key == 0)
+        //         {
+        //             $approver->status = 'Submitted';
+        //         }
+        //         elseif($key==1)
+        //         {
+        //             $approver->status = 'Pending';
+        //         }
+        //         else
+        //         {
+        //             $approver->status = 'Waiting';
+        //         }
+        //         $approver->save();
+        //     }
+        // }
 
         Alert::success('Successfully Updated')->persistent('Dismiss');
         return back();
